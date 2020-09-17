e564c402937dfabff115dc2cac71b4aa0354e691
Svanfridur Steingrimsdottir <svanfridursteingrimsdottir@MacBook-Pro.local>
2020-09-17 09:27:11 +0000

    Assignments

--- Verkefni í tímum/assigment4.1.py
+++
@@ -1,4 +0,0 @@
-upto = int(input("Input an int: "))  # Do not change this line
-
-for i in range(1,upto):
-    print(i)
--- Verkefni í tímum/assignment4.5.py
+++
@@ -1,7 +0,0 @@
-num = int(input("Input an int: ")) # Do not change this line
-count = 0
-
-for i in range(1,num+1):
-    count += i
-    for j in range(1):
-        print(count)
